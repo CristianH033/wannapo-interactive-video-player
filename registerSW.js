f723e21ca433836b9d5597b175139d74070c8c72
CristianH033 <CristianH033@users.noreply.github.com>
2024-07-10 13:33:17 +0000

    deploy: 2d23ad4a2c1aab53768ec9287f165332d5d0395c

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/wannapo-interactive-video-player/sw.js', { scope: '/wannapo-interactive-video-player/' })})}
