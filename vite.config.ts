2d23ad4a2c1aab53768ec9287f165332d5d0395c
Cristian David Home <cristian_david033@hotmail.com>
2024-07-10 08:30:21 -0500

    PWA config and Deploy config.

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/wannapo-interactive-video-player/',
   plugins: [
     Icons({ compiler: 'vue3' }),
     vue(),
